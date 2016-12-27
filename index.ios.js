ca8032cf38241c9b82cd4bd24da02120fcdabddd
Mateus Marques <marques.m05@gmail.com>
2016-12-26 23:56:18 -0200

    creating a header

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,12 @@
 // import a library
 import React from 'react';
-import { Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
 // crate a component 
+const App = () => (
+    <Header />
+  );
 
 // render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+
+// import libraries for making a component
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+// make a camponent
+const Header = () => {
+    return <Text>Albums!</Text>;
+};
+
+// make the component available to other parts of the app
+export default Header;
