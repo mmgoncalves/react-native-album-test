e7e53504ac0d13109cc9cf74e6bc738cced81839
Mateus Marques <marques.m05@gmail.com>
2016-12-29 10:44:58 -0200

    incluindo axios como dependencia

--- index.android.js
+++ index.android.js
@@ -1,12 +1,16 @@
 // import a library
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // crate a component 
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
 
 // render it to the device
-ReactNative.AppRegistry.registerComponent('album', () => App);
+AppRegistry.registerComponent('album', () => App);
